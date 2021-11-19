24646ece9b689bfb6ae272314658d369dd0d6afc
Alejandro Feijóo <alex13f13@gmail.com>
2021-11-19 17:23:30 +0100

    Nav

--- models/User.model.js
+++ models/User.model.js
@@ -3,10 +3,9 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    name: {
       type: String,
       require: true,
-      unique: true,
     },
     role: {
       type: String,
